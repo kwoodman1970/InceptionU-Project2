5b70cf66a834b737bc64519a30e353d55f048dfc
Zack Spring Chief <zackspringchief@Zacks-MacBook-Pro.local>
2023-04-24 13:29:55 -0600

    got activities to work

--- CalgaryFitFriends/src/main.jsx
+++ CalgaryFitFriends/src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App2 from "./App2";
 import App3 from "./App3";
+import App2 from "./App2";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
