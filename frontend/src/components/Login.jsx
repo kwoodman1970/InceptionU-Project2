5ffbc3d252372003efb22bbc576eabaccaf00425
Kevin Woodman <kwoodman1970@yahoo.ca>
2023-05-02 22:35:19 -0600

    Changed Home page in frontend to use distinct Login, Register and Logout components as applicable

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,40 +16,30 @@ export const Login = (props) => {
   return (
     <div className="auth-form-container">
       <h2>Login</h2>
-      {user != null ?
-        <>
-          <p>Currently logged in as <b>{user}</b></p>
-          <button type="submit" onClick={() => setUser(null)}>Log Out</button>
-        </> :
-        <>
-          <form className="login-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              placeholder="Your name"
-              id="name"
-              name="name"
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              value={pass}
-              onChange={(e) => setPass(e.target.value)}
-              type="password"
-              placeholder="*********"
-              id="password"
-              name="password"
-            />
-            <button type="submit">Log In</button>
-          </form>
-          <button
-            className="link-btn"
-            onClick={() => props.onFormSwitch("register")}
-          >
-            Don't have an account? Register here.
-          </button>
-        </>}
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Your name"
+          id="name"
+          name="name"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="*********"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <button className="link-btn" onClick={() => props.onFormSwitch("register")}>
+        Don't have an account? Register here.
+      </button>
     </div>
   );
 };
--- frontend/src/layouts/ActivitiesLayout.jsx
+++ frontend/src/layouts/ActivitiesLayout.jsx
@@ -12,12 +12,14 @@ export default function ActivitiesLayout() {
     <div className="activities-layout">
       <h2>Activities</h2>
       <p>Join or Create an activity!!!</p>
-      <p>Currently logged in as <b>{user}</b></p>
       {user != null ?
-        <nav>
-          <NavLink to="createactivities">Create Activity</NavLink>
-          <NavLink to="joinedactivities">Join Activity</NavLink>
-        </nav> :
+        <>
+          <p>Currently logged in as <b>{user}</b></p>
+          <nav>
+            <NavLink to="createactivities">Create Activity</NavLink>
+            <NavLink to="joinedactivities">Join Activity</NavLink>
+          </nav>
+        </> :
         <p>You aren't logged in.  Please <a href="/">log in</a> to see your activities.</p>
       }
       <Outlet />
